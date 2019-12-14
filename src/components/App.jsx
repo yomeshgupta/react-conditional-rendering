ff301700d66a3a4bab7a5291f4f35c1cdf56f79d
Yomesh Gupta <yomesh.gupta@gmail.com>
2019-12-14 18:25:49 +0530

    Added loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,43 @@
-import React, { Suspense, lazy, useState } from "react";
-import { useNetworkStatus } from "react-adaptive-hooks/network";
+import React, { Suspense, lazy, useState } from 'react';
+import { useNetworkStatus } from 'react-adaptive-hooks/network';
 
-const LoginBasic = lazy(() =>
-    import(/* webpackChunkName: "loginBasic" */ "./LoginBasic")
-);
-const LoginAdvanced = lazy(() =>
-    import(/* webpackChunkName: "loginAdvanced" */ "./LoginAdvanced")
-);
+const LoginBasic = lazy(() => import(/* webpackChunkName: "loginBasic" */ './LoginBasic'));
+const LoginAdvanced = lazy(() => import(/* webpackChunkName: "loginAdvanced" */ './LoginAdvanced'));
+
+function Loader() {
+	return (
+		<section className="loader">
+			<h1>Loading...</h1>
+		</section>
+	);
+}
 
 function Home({ onClick }) {
-    return (
-        <section className="home-wrapper">
-            <button onClick={() => onClick(state => !state)}>
-                Go to Login
-            </button>
-        </section>
-    );
+	return (
+		<section className="home-wrapper">
+			<button onClick={() => onClick(state => !state)}>Go to Login</button>
+		</section>
+	);
 }
 
 function App() {
-    const { effectiveConnectionType } = useNetworkStatus();
-    const [showLogin, toggleLogin] = useState(false);
+	const { effectiveConnectionType } = useNetworkStatus();
+	const [showLogin, toggleLogin] = useState(false);
 
-    if (!showLogin) return <Home onClick={toggleLogin} />;
+	if (!showLogin) return <Home onClick={toggleLogin} />;
 
-    function renderPage() {
-        const { search = "" } = window.location;
-        const queryParams = search.replace("?", "").split("&");
-        const showAdvanced =
-            queryParams.some(query => query.match(/mode=advance/, "i")) ||
-            effectiveConnectionType === "4g";
+	function renderPage() {
+		const { search = '' } = window.location;
+		const queryParams = search.replace('?', '').split('&');
+		const showAdvanced =
+			queryParams.some(query => query.match(/mode=advance/, 'i')) || effectiveConnectionType === '4g';
 
-        if (showAdvanced) return <LoginAdvanced />;
+		if (showAdvanced) return <LoginAdvanced />;
 
-        return <LoginBasic />;
-    }
+		return <LoginBasic />;
+	}
 
-    return <Suspense fallback={<div>Loading...</div>}>{renderPage()}</Suspense>;
+	return <Suspense fallback={<Loader />}>{renderPage()}</Suspense>;
 }
 
 export default App;
